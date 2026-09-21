266e56c913f7097452d289d4353725b278a5404f
ricardocosmico <jeffthepeep80@gmail.com>
2026-09-21 18:05:50 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,1 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hotel;
+
+/**
+ *
+ * @author ricardo
+ */
+public abstract class Pessoa {
+
+    private String id;
+    private String nome;
+    private String telefone;
+
+    public Pessoa(String id, String nome, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    // os getters retornam o valor do atributo correspondente 
+    // os setters permite modificar/atualizar as informaçoes presentes
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+}
+    
+
 
